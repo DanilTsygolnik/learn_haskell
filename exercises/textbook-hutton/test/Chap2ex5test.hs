def878987f09eae0e2c90e41d1890315330470ef
Danil Tsygolnik <tsygolnikd@gmail.com>
2023-06-27 21:35:50 +0300

    Hutton: add Chap2ex5 solution pt1 tests

---
+++ exercises/textbook-hutton/test/Chap2ex5test.hs
@@ -0,0 +1,18 @@
+module Chap2ex5test
+  (main) where
+
+import Test.Hspec
+import Chap2ex5
+
+main :: IO ()
+main = hspec $ do
+  describe "Chapter 2, exercise 4." $ do
+
+    it "initV1 ([1] :: [Int])      ---  ([] :: [Int])" $ do
+      initV1 ([1] :: [Int]) `shouldBe` ([] :: [Int])
+
+    it "initV1 ([1,2] :: [Int])    ---  ([1] :: [Int])" $ do
+      initV1 ([1,2] :: [Int]) `shouldBe` ([1] :: [Int])
+
+    it "initV1 ([1..10] :: [Int])  ---  ([1..9] :: [Int])" $ do
+      initV1 ([1..10] :: [Int]) `shouldBe` ([1..9] :: [Int])
