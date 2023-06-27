09994818fffdfc7bf4f971f1d05bb00ca7e8d684
Danil Tsygolnik <tsygolnikd@gmail.com>
2023-06-27 21:37:13 +0300

    Hutton: add Chap2ex5 solution pt2

--- exercises/textbook-hutton/sol-hs/Chap2ex5.hs
+++ exercises/textbook-hutton/sol-hs/Chap2ex5.hs
@@ -1,7 +1,11 @@
 module Chap2ex5
     ( initV1
+    , initV2
     )
     where
 
 initV1 :: [a] -> [a]
 initV1 xs = reverse (tail (reverse xs))
+
+initV2 :: [a] -> [a]
+initV2 xs = take (length xs - 1) xs
