d5745c8711c7399bf29b7f621a6d94294d970628
Danil Tsygolnik <tsygolnikd@gmail.com>
2023-06-27 21:40:28 +0300

    Hutton: add Chap2ex5 solution pt2 tests

--- exercises/textbook-hutton/test/Chap2ex5test.hs
+++ exercises/textbook-hutton/test/Chap2ex5test.hs
@@ -16,3 +16,12 @@ main = hspec $ do
 
     it "initV1 ([1..10] :: [Int])  ---  ([1..9] :: [Int])" $ do
       initV1 ([1..10] :: [Int]) `shouldBe` ([1..9] :: [Int])
+
+    it "initV2 ([1] :: [Int])      ---  ([] :: [Int])" $ do
+      initV2 ([1] :: [Int]) `shouldBe` ([] :: [Int])
+
+    it "initV2 ([1,2] :: [Int])    ---  ([1] :: [Int])" $ do
+      initV2 ([1,2] :: [Int]) `shouldBe` ([1] :: [Int])
+
+    it "initV2 ([1..10] :: [Int])  ---  ([1..9] :: [Int])" $ do
+      initV2 ([1..10] :: [Int]) `shouldBe` ([1..9] :: [Int])
