01cedffc202fd2275ad06ca452fbd3b6e67561c9
Danil Tsygolnik <tsygolnikd@gmail.com>
2023-06-27 21:13:10 +0300

    Hutton: add Chap2ex4 solution pt2 tests

--- exercises/textbook-hutton/test/Chap2ex4test.hs
+++ exercises/textbook-hutton/test/Chap2ex4test.hs
@@ -16,3 +16,12 @@ main = hspec $ do
 
     it "lastV1 ([1..10] :: [Int])  ---  (10 :: Int)" $ do
       lastV1 ([1..10] :: [Int]) `shouldBe` (10 :: Int)
+
+    it "lastV2 ([1] :: [Int])      ---  (1 :: Int)" $ do
+      lastV2 ([1] :: [Int]) `shouldBe` (1 :: Int)
+
+    it "lastV2 ([1,2] :: [Int])    ---  (2 :: Int)" $ do
+      lastV2 ([1,2] :: [Int]) `shouldBe` (2 :: Int)
+
+    it "lastV2 ([1..10] :: [Int])  ---  (10 :: Int)" $ do
+      lastV2 ([1..10] :: [Int]) `shouldBe` (10 :: Int)
